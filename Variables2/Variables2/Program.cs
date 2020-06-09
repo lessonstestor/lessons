9193e2540064ed605588b92b6ea502f626f42f66
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-09 17:06:23 +0300

    Add files via upload
    Variables zero

---
+++ Variables2/Variables2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variables2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 38 * 17;
+            int c = (31 - 5 * a) / b;
+            Console.WriteLine(c);
+            Console.Read();
+            // Ответ будет равен 0 так как int не может принемать значения с плавающей запятой.
+        }
+    }
+}
