994376845a64b752e7ff4d66558587c93e30d453
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-09 17:54:28 +0300

    Add files via upload
    Grannies

---
+++ Variables5/Variables5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeForOnePersonReseption = 10, grannies, hoursCounter, minutesCounter;
+
+            Console.Write("Введите кол-во старушек: ");
+
+            grannies = Convert.ToInt32(Console.ReadLine());
+
+            hoursCounter = grannies * timeForOnePersonReseption / 60;
+            minutesCounter = grannies * timeForOnePersonReseption - 60 * hoursCounter;
+
+            Console.WriteLine( "Вы должны отстоять в очереди: " + hoursCounter + " часа и " + minutesCounter + " минут" );
+            Console.Read();
+        }
+    }
+}
