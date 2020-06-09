ff7acf3b0aea75a6f8a4eb254b273aff10221aad
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-09 18:03:22 +0300

    Add files via upload
    Jobs

---
+++ Variables6/Variables6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, userZodiac, userJob;
+            int userAge;
+
+            Console.Write("Ваше имя: ");
+            userName = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваш знак зодиака: ");
+            userZodiac = Console.ReadLine();
+            Console.Write("Ваше место работы: ");
+            userJob = Console.ReadLine();
+            Console.WriteLine($"Вас зовут {userName}, возраст {userAge}, вы {userZodiac} и ваше место работы {userJob}");
+            Console.Read();
+        }
+    }
+}
