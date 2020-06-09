b3c763656b8fd1b61358d573588e2ed616203f71
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-09 17:15:05 +0300

    Add files via upload
    Variables - rows

---
+++ Variables3/Variables3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Variables3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inRow = 3, userImages = 52, maxRow;
+            maxRow = userImages / inRow;
+
+            Console.WriteLine("Количество полностью заполненных рядов = " + maxRow);
+            Console.Read();
+        }
+    }
+}
