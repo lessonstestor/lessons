24944f0c89ae59b496e2bb80f0bb44f240ac3d6c
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-09 16:47:15 +0300

    Add files via upload
    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue;
+            sbyte smallByteNumber;
+            byte byteNumber;
+            ushort ushortNumber;
+            short shortNumber;
+            int newNumber;
+            string beautifulString;
+            char symbol;
+            float dotNumber;
+            double doubleNumber;
+        }
+    }
+}
