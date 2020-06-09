6c5f0bc00bba5f5a1b8d4931877d04150541d108
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-09 17:35:52 +0300

    Add files via upload
    Gold Shop

---
+++ Variables4/Variables4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Variables4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float gold, crystalPrice = 10, userCrystals, convertGold, userGold; 
+
+            Console.Write( "Введите ваше количество золота: " );
+
+            gold = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine( "Цена кристалов " + crystalPrice );
+            Console.Write("Сумма золота для конвертации: ");
+
+            convertGold = Convert.ToSingle(Console.ReadLine());
+            userCrystals = crystalPrice / convertGold;
+            userGold = gold - convertGold;
+
+            Console.WriteLine( "В случае обмена количество криcталов равно " + userCrystals + " - остаток золота равен " + userGold);
+            Console.Read();
+        }
+    }
+}
