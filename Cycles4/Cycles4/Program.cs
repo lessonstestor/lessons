44a9369c0927dd33bc499ca78a19d0657f97faa5
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-10 18:26:22 +0300

    Add files via upload
    Password

---
+++ Cycles4/Cycles4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cycles4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userPassword = "2281448", successUserPasword;
+            int userAttemps = 3;
+
+            Console.WriteLine("Hello its a secret panel type your password: ");
+            while (userAttemps-- > 0)
+            {
+                successUserPasword = Console.ReadLine();
+                    
+                if (userPassword == successUserPasword)
+                {
+                    Console.WriteLine("Hello from Dark_Club");
+                }
+                else
+                {
+                    Console.WriteLine("Try again please. Your attemps: " + userAttemps);
+                }
+            }
+
+        }
+    }
+}
