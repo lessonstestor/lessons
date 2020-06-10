ff1300184de4f70f26d167d6798ac5f5a4675488
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-10 18:14:35 +0300

    Add files via upload
    Name Symbol

---
+++ Cycles3/Cycles3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Cycles3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, userSymbol;
+            Console.WriteLine("Please write your name...");
+            userName = Console.ReadLine();
+            Console.WriteLine("Your name: " + userName);
+
+            Console.WriteLine("Please write your symbol...");
+            userSymbol = Console.ReadLine();
+            Console.WriteLine("Your symbol: " + userSymbol);
+
+            Console.WriteLine("Your result:");
+            for (int i = 0; i < userName.Length+2; i++)
+            {
+                Console.Write(userSymbol);
+            }
+            Console.WriteLine();
+            Console.WriteLine(userSymbol + userName + userSymbol);
+            for (int i = 0; i < userName.Length+2; i++)
+            {
+                Console.Write(userSymbol);
+            }
+            Console.Read();
+        }
+    }
+}
