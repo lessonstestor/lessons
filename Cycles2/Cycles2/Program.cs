6cc80904c66003192f874a1f574fcf770b9ce96b
lessonstestor <66682294+lessonstestor@users.noreply.github.com>
2020-06-10 17:59:16 +0300

    Add files via upload
    use for

---
+++ Cycles2/Cycles2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cycles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Используем for так как знаем конечное значение цикла
+             (while используем если неуверенны, когда конкретно должен завершиться цикл)
+             */
+            for (int i = 0; i <= 98; i++)
+            {
+                if (i==0)
+                {
+                    continue;
+                }
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
